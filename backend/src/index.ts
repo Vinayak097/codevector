0a3dfb6f74be0161fed8139e2eb55ac9bc289e6b
unknown <vinayak20injamure@gmail.com>
2026-06-24 12:33:25 +0530

    dotenv added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors'
-
+import dotenv from 'dotenv'
+dotenv.config()
 import { getProducts } from "./controllers/product.controller";
 
 const app = express();
